be26601fb141e2e40c255bf3a94577ec7e3f5de2
ThuHocIT <na.thu06277@gmail.com>
2023-05-24 17:43:56 +0700

    update mini project

--- mini-project/baitapnho.asm
+++ mini-project/baitapnho.asm
@@ -1,49 +1,56 @@
 .data
-sequence:   .word   1, 3, 2, 1     # Đặt dãy số trong mảng sequence
-length:     .word   4               # Độ dài của dãy số
-
+sequence:   .word   1, 2, 4, 4, 5    # Đặt dãy số trong mảng sequence
+length:     .word   5              # Độ dài của dãy số
+Message: .asciiz "Day co the la day tang ngat: "
+True: .asciiz "true"
+False: .asciiz "false"
 .text
-.globl  main
+#.globl  main
 
 main:
     la      $t0, sequence           # Lấy địa chỉ của mảng sequence
     lw      $t1, length             # Lấy độ dài của dãy số
     addi    $t1, $t1, -1            # Trừ đi 1 để tính index cuối cùng
 
-    jal     almostIncreasingSequence # Gọi hàm almostIncreasingSequence
+    j  	     almostIncreasingSequence # Gọi hàm almostIncreasingSequence  al 
 
-    li      $v0, 10                 # Exit program
-    syscall
-
+#Hàm in kết quả đúng
+true:
+   	li $v0, 59
+ 	la $a0, Message
+ 	la $a1, True 
+ 	syscall
+endtaskwithtrue:
+ 	li      $v0, 10                 # Exit program
+    	syscall 
 # Hàm kiểm tra dãy số tăng gần đúng
 almostIncreasingSequence:
-    addi    $sp, $sp, -8            # Cấp phát không gian cho biến cục bộ
-    sw      $ra, 4($sp)             # Lưu trữ địa chỉ trở về
-
-    move    $s0, $t1                # Sao chép index cuối cùng vào $s0
     li      $t2, 0                  # Đặt biến đếm là 0
 
+#Hàm vòng lặp chính
 checkIncreasing:
-    beq     $t1, $zero, checkRemoval    # Kiểm tra nếu index đã đạt đến đầu dãy số
+    beq     $t1, $zero, true    # Kiểm tra nếu index đã đạt đến đầu dãy số mà vẫn hợp lệ, nhảy đến true
 
     lw      $t3, 0($t0)             # Lấy giá trị tại index hiện tại
     lw      $t4, 4($t0)             # Lấy giá trị tại index kế tiếp
 
-    ble     $t3, $t4, checkRemoval      # Kiểm tra nếu dãy số tăng
+    bgt      $t4, $t3, checkRemoval      # Kiểm tra nếu dãy số tăng (a[i+1] > a[i]
 
     addi    $t2, $t2, 1            # Tăng biến đếm lỗi
 
-    # Kiểm tra nếu có hơn 1 lỗi, quay trở lại main
-    bgt     $t2, 1, returnMain     
+    # Kiểm tra nếu có hơn 1 lỗi, chạy đến hàm false
+    bgt     $t2, 1, false
 
+#Hàm cập nhật giá trị	        
 checkRemoval:
     addi    $t1, $t1, -1            # Giảm index đi 1
     addi    $t0, $t0, 4             # Đi đến index tiếp theo
-
     j       checkIncreasing         # Quay lại vòng lặp kiểm tra
-
-returnMain:
-    lw      $ra, 4($sp)             # Lấy địa chỉ trở về
-    addi    $sp, $sp, 8             # Giải phóng không gian biến cục bộ
-    jr      $ra                     # Trở về địa chỉ gọi
+    
+#Hàm in kết quả sai
+false:
+    li $v0, 59
+    la $a0, Message
+    la $a1, False
+    syscall
 
